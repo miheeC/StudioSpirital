f74825f7f733f0732705ea2bc5285b014ae8b3d1
miheeC <miha.kegl@gmail.com>
2026-06-26 10:16:27 +0200

    style changes

---
+++ js/lightbox.js
@@ -0,0 +1,27 @@
+(function () {
+  var lightbox = document.getElementById('lightbox');
+  var lightboxImg = lightbox.querySelector('.lightbox-img');
+
+  document.querySelectorAll('.gallery-img').forEach(function (img) {
+    img.addEventListener('click', function () {
+      lightboxImg.src = img.src;
+      lightboxImg.alt = img.alt;
+      lightbox.classList.add('is-open');
+      lightbox.setAttribute('aria-hidden', 'false');
+    });
+  });
+
+  function close() {
+    lightbox.classList.remove('is-open');
+    lightbox.setAttribute('aria-hidden', 'true');
+    lightboxImg.src = '';
+  }
+
+  lightbox.addEventListener('click', function (e) {
+    if (e.target !== lightboxImg) close();
+  });
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') close();
+  });
+})();
